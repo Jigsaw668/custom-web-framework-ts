2e4334feaeb934783d2e7a4fe5daed2499a67030
Denis Makarov <denis.makarov@intravision.ru>
2020-12-12 13:30:19 +0300

    create User model

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-console.log('hi');
+import { User } from './models/User';
+
+const user = new User({ name: 'name', age: 25 });
+
+user.on('change', () => {
+  console.log('Change 1');
+});
+user.on('change', () => {
+  console.log('Change 2');
+});
+user.on('save', () => {
+  console.log('Save');
+});
+
+user.trigger('change');
+
+console.log(user);
