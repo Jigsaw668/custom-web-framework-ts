d7c48bdfb303be066e9185cecbc1107b0d7546e2
Denis Makarov <denis.makarov@intravision.ru>
2020-12-18 10:53:37 +0300

    init tsc config strict mode

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,16 @@ import { User } from './models/User';
 
 const user = User.buildUser({ name: 'NAME', age: 20 });
 
-const userForm = new UserForm(
-  document.querySelector('#root'),
-  user,
-);
+const root = document.querySelector('#root');
 
-userForm.render();
+if (root) {
+  const userForm = new UserForm(
+    root,
+    user,
+  );
+  
+  userForm.render();
+} else {
+  throw new Error('Root element not found');
+  
+}
--- src/views/UserForm.ts
+++ src/views/UserForm.ts
@@ -14,11 +14,22 @@ export class UserForm {
   eventsMap(): { [key: string]: () => void } {
     return {
       'click:.set-age': this.onSetAgeClick,
+      'click:.set-name': this.onSetNameClick,
     }
   }
 
   onSetAgeClick = (): void => {
     this.model.setRandomAge();
+  }
+
+  onSetNameClick = (): void => {
+    const input = this.parent.querySelector('input');
+
+    if (input) {
+      const name = input.value;
+
+      this.model.set({ name });
+    }
   }
 
   template(): string {
@@ -28,7 +39,7 @@ export class UserForm {
         <div>User name: ${this.model.get('name')}</div>
         <div>User age: ${this.model.get('age')}</div>
         <input />
-        <button>Change name</button>
+        <button class='set-name'>Change name</button>
         <button class='set-age'>Set Random Age</button>
       </div>
     `;
