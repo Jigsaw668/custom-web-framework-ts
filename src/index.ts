3673e51c7d0c155df0900698c7b501ea9fea81eb
Denis Makarov <denis.makarov@intravision.ru>
2020-12-22 15:18:40 +0600

    render nesting components (regions map)

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { UserForm } from './views/UserForm';
+import { UserEdit } from './views/UserEdit';
 import { User } from './models/User';
 
 const user = User.buildUser({ name: 'NAME', age: 20 });
@@ -6,12 +6,14 @@ const user = User.buildUser({ name: 'NAME', age: 20 });
 const root = document.querySelector('#root');
 
 if (root) {
-  const userForm = new UserForm(
+  const userEdit = new UserEdit(
     root,
     user,
   );
   
-  userForm.render();
+  userEdit.render();
+
+  console.log(userEdit);
 } else {
   throw new Error('Root element not found');
 }
