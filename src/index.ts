61cfe4977379e36f099fdf1386115ebf2306ff04
Denis Makarov <denis.makarov@intravision.ru>
2020-12-22 15:50:39 +0600

    implement collection users

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,35 @@
+import { Collection } from './models/Collection';
+import { User, UserProps} from './models/User';
 import { UserEdit } from './views/UserEdit';
-import { User } from './models/User';
+import { UserList } from './views/UserList';
 
 const user = User.buildUser({ name: 'NAME', age: 20 });
 
-const root = document.querySelector('#root');
+const userElement = document.querySelector('#user');
 
-if (root) {
-  const userEdit = new UserEdit(
-    root,
-    user,
-  );
-  
+if (userElement) {
+  const userEdit = new UserEdit(userElement, user);
+
   userEdit.render();
-
-  console.log(userEdit);
 } else {
-  throw new Error('Root element not found');
+  throw new Error('user element not found');
 }
+
+
+const userListElement = document.querySelector('#user-list');
+
+const users = new Collection('http://localhost:3000/users',
+  (json: UserProps) => {
+    return User.buildUser(json);
+  }
+);
+
+users.on('change', () => {
+  if (userListElement) {
+    new UserList(userListElement, users).render();
+  } else {
+    throw new Error('user list element not found');
+  }
+});
+
+users.fetch();
